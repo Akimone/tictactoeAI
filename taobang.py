f506788aea902a4f3fabc2c51d0505fb5704fda0
Akimone <boycute2002@gmail.com>
2023-08-25 19:43:03 +0700

    tictactoeAI

---
+++ taobang.py
@@ -0,0 +1,27 @@
+# ---------
+# TẠO BẢNG DISPLAY
+# ---------
+
+# --- TẠO ĐIỂM ẢNH ---
+
+WIDTH = 1000
+HEIGHT = WIDTH
+
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS
+
+LINE_WIDTH = 15
+CIRC_WIDTH = 15
+CROSS_WIDTH = 20
+
+RADIUS = SQSIZE // 4
+
+OFFSET = 50
+
+# --- MÀU ---
+
+BG_COLOR = (0, 170, 255)
+LINE_COLOR = (23, 145, 135)
+CIRC_COLOR = (240, 178, 122)
+CROSS_COLOR = (125, 60, 152 )
